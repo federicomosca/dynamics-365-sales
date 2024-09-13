c60be56d24ec4f80e7d4831a65cc6f7e240d575b
Federico Mosca <f.mosca@xrmhub.it>
2024-09-13 17:09:35 +0200

    Deposito PreCreate Adress

---
+++ RSMNG.TAUMEDIKA.Plugins/res_Address/PreCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSMNG.TAUMEDIKA.Plugins.res_Address
+{
+    class PreCreate : RSMNG.BaseClass
+    {
+        public PreCreate(string unsecureConfig, string secureConfig) : base(unsecureConfig, secureConfig)
+        {
+            PluginStage = Stage.PRE;
+            PluginMessage = "Create";
+            PluginPrimaryEntityName = DataModel.res_address.logicalName;
+            PluginRegion = "";
+            PluginActiveTrace = false;
+        }
+        public override void ExecutePlugin(CrmServiceProvider crmServiceProvider)
+        {
+            Entity target = (Entity)crmServiceProvider.PluginContext.InputParameters["Target"];
+
+            #region Genera
+            PluginRegion = "Funzione1";
+
+            #endregion
+        }
+    }
+}
+
