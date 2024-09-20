7c75d8312e2a3e88f6ab48ae3271e89b5239eca2
Federico Mosca <f.mosca@xrmhub.it>
2024-09-20 17:22:51 +0200

    quote > preupdate > implementata logica su tutti i campi della sezione "totals"

--- RSMNG.TAUMEDIKA.Plugins/Quote/PreUpdate.cs
+++ RSMNG.TAUMEDIKA.Plugins/Quote/PreUpdate.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
+using RSMNG.TAUMEDIKA.DataModel;
 using RSMNG.TAUMEDIKA.Shared.Country;
 using System;
 using System.Collections.Generic;
@@ -23,8 +24,99 @@ namespace RSMNG.TAUMEDIKA.Plugins.Quote
         public override void ExecutePlugin(CrmServiceProvider crmServiceProvider)
         {
             Entity target = (Entity)crmServiceProvider.PluginContext.InputParameters["Target"];
+            crmServiceProvider.PluginContext.PreEntityImages.TryGetValue("PreImage", out Entity preImage);
+            if (preImage == null) { return; }
+
+            if (target.Contains(quote.totallineitemamount) ||
+                target.Contains(quote.totaldiscountamount) ||
+                target.Contains(quote.totaltax) ||
+                target.Contains(quote.freightamount) ||
+                target.Contains(quote.res_vatnumberid) ||
+                target.Contains(quote.freightamount)
+                )
+            {
+                decimal taxableAmountSum = 0;     // Somma 'Totale Imponibile' righe ordine
+                decimal taxRowsSum = 0;
+                decimal freightAmount = 0;
+                decimal totalDiscountAmount = 0; // Sconto totale
+                decimal rateVatNumber = 0;
+                decimal freightAmountRate = 0;
+                decimal totalAmountlessFreight = 0; // Totale imponibile
+                bool isTrace = true;
 
 
+                //----Importo Spesa Accessoria
+                Money freightAmountMoney = target.Contains(quote.freightamount) ? target.GetAttributeValue<Money>(quote.freightamount) : preImage.GetAttributeValue<Money>(quote.freightamount);
+                freightAmount = freightAmountMoney != null ? freightAmountMoney.Value : 0;
+
+                //----Recupera Aliquota Codice IVA Spesa Accessoria
+                EntityReference erVatNumber = target.Contains(quote.res_vatnumberid) ? target.GetAttributeValue<EntityReference>(quote.res_vatnumberid) : preImage.GetAttributeValue<EntityReference>(quote.res_vatnumberid);
+
+                if (freightAmount != 0 && erVatNumber != null)
+                {
+                    Entity enVatNumber = crmServiceProvider.Service.Retrieve(res_vatnumber.logicalName, erVatNumber.Id, new Microsoft.Xrm.Sdk.Query.ColumnSet(new string[] { res_vatnumber.res_rate }));
+
+                    rateVatNumber = enVatNumber.ContainsAttributeNotNull(res_vatnumber.res_rate) ? enVatNumber.GetAttributeValue<decimal>(res_vatnumber.res_rate) : 0;
+
+                    freightAmountRate = freightAmount * (rateVatNumber / 100);
+                }
+
+
+                //----Recupera somme aggregati su Righe Ordine
+                var fetchData = new
+                {
+                    quoteid = target.Id,
+                };
+                var fetchXml = $@"<?xml version=""1.0"" encoding=""utf-16""?>
+                            <fetch aggregate=""true"">
+                              <entity name=""{quotedetail.logicalName}"">
+                                <attribute name=""{quotedetail.res_taxableamount}"" alias=""taxableAmount"" aggregate=""sum"" />
+                                <attribute name=""{quotedetail.manualdiscountamount}"" alias=""ManualDiscountAmount"" aggregate=""sum"" />
+                                <attribute name=""{quotedetail.tax}"" alias=""Tax"" aggregate=""sum"" />
+                                <filter>
+                                  <condition attribute=""{quotedetail.quoteid}"" operator=""eq"" value=""{fetchData.quoteid}"" />
+                                </filter>
+                              </entity>
+                            </fetch>";
+                if (isTrace) { crmServiceProvider.TracingService.Trace(fetchXml); }
+
+                EntityCollection ecSum = crmServiceProvider.Service.RetrieveMultiple(new FetchExpression(fetchXml));
+
+                if (ecSum != null)
+                {
+                    taxableAmountSum = ecSum[0].ContainsAliasNotNull("taxableAmount") ? ecSum[0].GetAliasedValue<Money>("taxableAmount").Value : 0;
+                    taxRowsSum = ecSum[0].ContainsAliasNotNull("Tax") ? ecSum[0].GetAliasedValue<Money>("Tax").Value : 0;
+                    totalDiscountAmount = ecSum[0].ContainsAliasNotNull("ManualDiscountAmount") ? ecSum[0].GetAliasedValue<Money>("ManualDiscountAmount").Value : 0;
+
+                    if (isTrace)
+                    {
+                        crmServiceProvider.TracingService.Trace("totalDiscountAmount: " + taxableAmountSum.ToString() + "\n" +
+                                                                            "taxRowsSum: " + taxRowsSum);
+                    }
+                }
+
+                totalAmountlessFreight = taxableAmountSum - totalDiscountAmount; // Totale imponibile
+                decimal totalTax = taxRowsSum + freightAmountRate;
+                decimal totalAmount = totalAmountlessFreight + totalTax;
+
+                target[quote.totallineitemamount] = taxableAmountSum != 0 ? new Money(taxableAmountSum) : null; // Totale Righe = Somma totale imponibile righe
+                target[quote.totalamountlessfreight] = totalAmountlessFreight != 0 ? new Money(totalAmountlessFreight) : null;
+                target[quote.totaldiscountamount] = totalDiscountAmount != 0 ? new Money(totalDiscountAmount) : null;   // Somma Sconto Totale righe
+                target[quote.totaltax] = (totalTax) != 0 ? new Money(totalTax) : null;
+
+                target[quote.totalamount] = (totalAmount) != 0 ? new Money(totalAmount) : null;
+
+                if (isTrace)
+                {
+                    crmServiceProvider.TracingService.Trace(
+                        "totallineitemamount: " + taxableAmountSum.ToString() + "\n" +
+                        "totalamountlessfreight: " + totalAmountlessFreight.ToString() + "\n" +
+                        "totaltax: " + totalTax.ToString() + "\n" +
+                        "totalamount: " + totalAmount.ToString()
+
+                        );
+                }
+            }
         }
     }
 }
