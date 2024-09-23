30670111ae68f8f5170940c5ba3919aac22e7ec2
Federico Mosca <f.mosca@xrmhub.it>
2024-09-23 17:29:55 +0200

    quote > all'acquisizione dell'offerta, viene generato un ordine con i campi in comune già valorizzati

--- RSMNG.TAUMEDIKA.Plugins/Quote/PostCreate.cs
+++ RSMNG.TAUMEDIKA.Plugins/Quote/PostCreate.cs
@@ -33,6 +33,58 @@ namespace RSMNG.TAUMEDIKA.Plugins.Quote
 
             //crmServiceProvider.Service.Update(target);
             #endregion
+
+            #region Creo l'ordine con Motivo Stato = "Approvato"
+            PluginRegion = "Creo l'ordine con Motivo Stato = \"Approvato\"";
+
+            EntityReference erQuote = target.ToEntityReference();
+
+            ColumnSet quoteColumnSet = new ColumnSet(
+                DataModel.quote.name,
+                DataModel.quote.transactioncurrencyid,
+                DataModel.quote.pricelevelid,
+                DataModel.quote.res_date,
+                DataModel.quote.res_isinvoicerequested,
+                "res_paymenttermid",
+                DataModel.quote.res_deposit,
+                DataModel.quote.res_vatnumberid,
+                DataModel.quote.res_additionalexpenseid,
+                DataModel.quote.willcall,
+                DataModel.quote.res_shippingreference,
+                DataModel.quote.shipto_line1,
+                DataModel.quote.shipto_postalcode,
+                DataModel.quote.shipto_city,
+                DataModel.quote.res_location,
+                DataModel.quote.shipto_stateorprovince,
+                DataModel.quote.res_countryid,
+                DataModel.quote.totallineitemamount,
+                DataModel.quote.totalamountlessfreight,
+                DataModel.quote.freightamount,
+                DataModel.quote.totaltax,
+                DataModel.quote.totalamount,
+                DataModel.quote.totaldiscountamount,
+                DataModel.quote.opportunityid,
+                DataModel.quote.customerid,
+                DataModel.quote.description,
+                DataModel.quote.res_internalusecomment
+                );
+
+            Entity salesorder = new Entity(DataModel.salesorder.logicalName);
+
+            foreach (string column in quoteColumnSet.Columns)
+            {
+                if (target.Contains(column) && target.GetAttributeValue<object>(column) != null)
+                {
+                    salesorder[column] = target.GetAttributeValue<object>(column);
+                }
+            }
+
+            salesorder[DataModel.salesorder.quoteid] = erQuote;
+            salesorder[DataModel.salesorder.statecode] = 0;
+            salesorder[DataModel.salesorder.statuscode] = 100005;
+
+            crmServiceProvider.Service.Create(salesorder);
+            #endregion
         }
     }
 }
