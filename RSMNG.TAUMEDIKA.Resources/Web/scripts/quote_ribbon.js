882eb0abc8403f2019cc5671f05339a5b5559689
Federico Mosca <f.mosca@xrmhub.it>
2024-09-26 16:19:37 +0200

    quote > ribbon > gestita la visiblità dei bottoni sul form quote

--- RSMNG.TAUMEDIKA.Resources/Web/scripts/quote_ribbon.js
+++ RSMNG.TAUMEDIKA.Resources/Web/scripts/quote_ribbon.js
@@ -68,51 +68,29 @@ if (typeof (RSMNG.TAUMEDIKA.QUOTE.RIBBON.HOME) == "undefined") {
                 _self.Agent = await _self.getAgent();
             }
             switch (status) {
-                //in approvazione
-                case "APPROVAL":
-                    if (currentStatus === _self.STATUS.BOZZA) {
-                        if (_self.Agent === 1) { visible = true; }
-                    }
-                    break;
-                //approvata
-                case "APPROVED":
-                    if (currentStatus === _self.STATUS.BOZZA ||
-                        (currentStatus === _self.STATUS.IN_APPROVAZIONE && _self.Agent === 0)) {
-                        visible = true;
-                    }
-                    if (currentStatus === _self.STATUS.IN_APPROVAZIONE) {
-                        visible = true;
-                    }
-                    break;
-                //non approvata
-                case "NOT_APPROVED":
-                    if (currentStatus === _self.STATUS.IN_APPROVAZIONE && _self.Agent === 0) {
-                        visible = true;
-                    }
-                    break;
-                //chiudi offerta (persa)
-                case "CLOSE_QUOTE":
-                    if (currentStatus === _self.STATUS.APPROVATA) {
-                        visible = true;
-                    }
-                    break;
-                //acquisisci offerta (acquisita)
-                case "CREATE_ORDER":
-                    if (currentStatus === _self.STATUS.APPROVATA) {
-                        visible = true;
-                    }
-                    break;
-                //aggiorna
-                case "REVISE":
-                    if (currentStatus === _self.STATUS.IN_APPROVAZIONE ||
-                        currentStatus === _self.STATUS.APPROVATA) {
-                        visible = true;
-                    }
-                    break;
-                //
-                case "ACTIVATE_QUOTE":
 
-                    break;
+                case "APPROVAL": //in approvazione
+                    if (currentStatus === _self.STATUS.BOZZA && _self.Agent === 1) { visible = true; } break;
+
+                case "APPROVED": //approvata
+                    if (currentStatus === _self.STATUS.BOZZA && _self.Agent === 0 || _self.Agent == null) { visible = true; }
+                    if (currentStatus === _self.STATUS.IN_APPROVAZIONE && _self.Agente === 0 || _self.Agent == null) { visible = true; } break;
+
+                case "NOT_APPROVED": //non approvata
+                    if (currentStatus === _self.STATUS.IN_APPROVAZIONE && _self.Agent === 0 || _self.Agent == null) { visible = true; } break;
+
+                case "CREATE_ORDER": //acquisisci offerta (acquisita)
+                    if (currentStatus === _self.STATUS.APPROVATA) { visible = true; } break;
+
+                case "CLOSE_QUOTE": //chiudi offerta (persa)
+                    if (currentStatus === _self.STATUS.APPROVATA) { visible = true; } break;
+
+                case "REVISE": //aggiorna
+                    if (currentStatus === _self.STATUS.IN_APPROVAZIONE) { visible = true; }
+                    if (currentStatus === _self.STATUS.APPROVATA) { visible = true; } break;
+
+                case "ACTIVATE_QUOTE": // attiva
+                    if (currentStatus === _self.STATUS.BOZZA) { visible = true; } break;
             }
             return visible;
         },
