24e2f9612961852e8b1b1cfb50dd19805ace0aaf
Federico Mosca <f.mosca@xrmhub.it>
2024-10-02 09:07:01 +0200

    aggiungo NOT_APPROVED tra i casi della chiamata alla Client Action

--- RSMNG.TAUMEDIKA.Resources/Web/scripts/quote_ribbon.js
+++ RSMNG.TAUMEDIKA.Resources/Web/scripts/quote_ribbon.js
@@ -149,14 +149,15 @@ if (typeof (RSMNG.TAUMEDIKA.QUOTE.RIBBON.HOME) == "undefined") {
                     console.log(`case: ${status}`)
                     console.log(`statuscode: ${quoteStatus}`);
 
-                    //const actionName = "UPDATE_QUOTE_STATUS";
+                    const actionName = "UPDATE_QUOTE_STATUS";
 
-                    //const success = RSMNG.TAUMEDIKA.GLOBAL.invokeClientAction(quoteId, status, actionName);
-                    //if (!success) {
-
-                    //formNotification
-                    //formContext.ui.setFormNotification("Impossibile aggiornare lo stato del record", "ERROR", "01");
-                    //}
+                    const success = RSMNG.TAUMEDIKA.GLOBAL.invokeClientAction(quoteId, status, actionName)
+                        .then(result => {
+                            console.log("Action executed successfully");
+                        })
+                        .catch(error => {
+                            console.error("Error executing action");
+                        });
                     break;
             }
         }
