d772a92f8a9c611cd8e46d10f37d7e3deb1e8863
Kostyantyn Fois Makarov <k.foismakarov@rsmanagement.it>
2024-10-11 09:07:43 +0200

    Deposito res_commission_ribbon.js, systemuser.js, selectAgents.html

---
+++ RSMNG.TAUMEDIKA.Resources/Web/scripts/res_commission_ribbon.js
@@ -0,0 +1,121 @@
+﻿
+if (typeof (RSMNG) == "undefined") {
+    RSMNG = {};
+}
+
+if (typeof (RSMNG.TAUMEDIKA) == "undefined") {
+    RSMNG.TAUMEDIKA = {};
+}
+
+if (typeof (RSMNG.TAUMEDIKA.COMMISSION) == "undefined") {
+    RSMNG.TAUMEDIKA.COMMISSION = {};
+}
+
+if (typeof (RSMNG.TAUMEDIKA.COMMISSION.RIBBON) == "undefined") {
+    RSMNG.TAUMEDIKA.COMMISSION.RIBBON = {};
+}
+
+if (typeof (RSMNG.TAUMEDIKA.COMMISSION.RIBBON.FORM) == "undefined") {
+    RSMNG.TAUMEDIKA.COMMISSION.RIBBON.FORM = {};
+}
+
+if (typeof (RSMNG.TAUMEDIKA.COMMISSION.RIBBON.SUBGRID) == "undefined") {
+    RSMNG.TAUMEDIKA.COMMISSION.RIBBON.SUBGRID = {};
+}
+
+if (typeof (RSMNG.TAUMEDIKA.COMMISSION.RIBBON.HOME) == "undefined") {
+    RSMNG.TAUMEDIKA.COMMISSION.RIBBON.HOME = {};
+}
+
+(function () {
+
+    var _self = this;
+
+	_self.formModel = {
+		entity: {
+			///Provvigione constants.
+			logicalName: "res_commission",
+			displayName: "Provvigione",
+		},
+		fields: {
+
+			///Provvigione
+			res_commissionid: "res_commissionid",
+			///Data fine
+			res_enddate: "res_enddate",
+			///Nome
+			res_name: "res_name",
+			///Data inizio
+			res_startdate: "res_startdate",
+
+			/// Values for field Stato
+			statecodeValues: {
+				Attivo: 0,
+				Inattivo: 1
+			},
+
+			/// Values for field Motivo stato
+			statuscodeValues: {
+				Bozza_StateAttivo: 1,
+				Calcolata_StateAttivo: 100000002,
+				Calcolataerrori_StateAttivo: 100000003,
+				Calcoloincorso_StateAttivo: 100000001,
+				Inattivo_StateInattivo: 2
+			}
+		}
+	};
+
+
+    //--------------------------------------------------
+    _self.STARTCALC = {
+        canExecute: async function (formContext) {
+
+            let statuscode = formContext.getAttribute("statuscode").getValue();
+
+            return statuscode != _self.formModel.fields.statuscodeValues.Calcoloincorso_StateAttivo ? true : false;
+        },
+        execute: async function (formContext) {
+
+            //await import('../res_scripts/res_global.js');
+
+            pageInput = {
+                pageType: 'webresource',
+                webresourceName: '/res_pages/selectAgents.html'
+            }
+
+            navigationOptions = {
+                target: 2,
+                width: { value: 600, unit: "px" },
+                height: { value: 660, unit: "px" },
+                position: 1,
+                title: 'Seleziona Agenti'
+            }
+            window._formContext = formContext;
+            Xrm.Navigation.navigateTo(pageInput, navigationOptions).then(
+                function (result) {
+
+                    if (result.returnValue != null) {
+
+                        console.log("navigate ok");
+
+                    }
+                },
+
+                function (error) {
+                    console.log(error.message);
+                }
+            );
+        }
+    };
+}).call(RSMNG.TAUMEDIKA.COMMISSION.RIBBON.FORM);
+
+
+
+
+
+/*
+Alla call puoi aggiungere i namespace se hai necessità di estendere le funzionalità
+- RSMNG.PROGETTO.ENTITY.RIBBON.SUBGRID
+- RSMNG.PROGETTO.ENTITY.RIBBON.HOME
+*/
+
---
+++ RSMNG.TAUMEDIKA.Resources/Web/scripts/systemuser.js
@@ -0,0 +1,115 @@
+﻿//sostituire PROGETTO con nome progetto
+//sostituire ENTITY con nome entità
+if (typeof (RSMNG) == "undefined") {
+    RSMNG = {};
+}
+
+if (typeof (RSMNG.TAUMEDIKA) == "undefined") {
+    RSMNG.TAUMEDIKA = {};
+}
+
+if (typeof (RSMNG.TAUMEDIKA.SYSTEMUSER) == "undefined") {
+    RSMNG.TAUMEDIKA.SYSTEMUSER = {};
+}
+
+(function () {
+    var _self = this;
+
+    //Form model
+    _self.formModel = {
+        entity: {
+            ///Utente constants.
+            logicalName: "systemuser",
+            displayName: "Utente",
+        },
+        fields: {
+            ///Codice agente
+            res_agentnumber: "res_agentnumber",
+            ///Agente
+            res_isagente: "res_isagente",
+            ///Disabilita calcolo provvigioni
+            res_iscommissioncalculationdisabled: "res_iscommissioncalculationdisabled",
+        },
+        tabs: {
+
+        },
+        sections: {
+
+        }
+    };
+
+    _self.onSaveForm = function (executionContext) {
+        if (executionContext.getEventArgs().getSaveMode() == 70) {
+            executionContext.getEventArgs().preventDefault();
+            return;
+        }
+    };
+    //---------------------------------------------------
+    _self.onLoadCreateForm = async function (executionContext) {
+
+        var formContext = executionContext.getFormContext();
+
+    };
+    //---------------------------------------------------
+    _self.onLoadUpdateForm = async function (executionContext) {
+
+        var formContext = executionContext.getFormContext();
+
+
+    };
+    //---------------------------------------------------
+    _self.onLoadReadyOnlyForm = function (executionContext) {
+
+        var formContext = executionContext.getFormContext();
+    };
+    //---------------------------------------------------
+    _self.onChangeIsAgente = function (executionContext, isEvent) {
+        var formContext = executionContext.getFormContext();
+
+        let isAgente = formContext.getAttribute(_self.formModel.fields.res_isagente).getValue();
+
+        formContext.getControl(_self.formModel.fields.res_agentnumber).setDisabled(isAgente == true ? false : true);
+        formContext.getAttribute(_self.formModel.fields.res_agentnumber).setRequiredLevel(isAgente == true ?  "required" : "none");
+
+        formContext.getControl(_self.formModel.fields.res_iscommissioncalculationdisabled).setVisible(isAgente == true ? true : false);
+        formContext.getControl(_self.formModel.fields.res_iscommissioncalculationdisabled).setDisabled(isAgente == true ? false : true);
+
+
+        if (isEvent == true && isAgente == false) {
+            formContext.getAttribute(_self.formModel.fields.res_agentnumber).setValue(null);
+            formContext.getAttribute(_self.formModel.fields.res_iscommissioncalculationdisabled).setValue(false);
+
+        }
+    }
+    //---------------------------------------------------
+    _self.onLoadForm = async function (executionContext) {
+
+
+        await import('../res_scripts/res_global.js');
+
+        //init formContext
+        var formContext = executionContext.getFormContext();
+
+        //Init event
+        formContext.getAttribute(_self.formModel.fields.res_isagente).addOnChange(() => { _self.onChangeIsAgente(executionContext, true); });
+
+        //Init function
+        _self.onChangeIsAgente(executionContext, false);
+
+        switch (formContext.ui.getFormType()) {
+            case RSMNG.Global.CRM_FORM_TYPE_CREATE:
+                _self.onLoadCreateForm(executionContext);
+                break;
+            case RSMNG.Global.CRM_FORM_TYPE_UPDATE:
+                _self.onLoadUpdateForm(executionContext);
+                break;
+            case RSMNG.Global.CRM_FORM_TYPE_READONLY:
+                _self.onLoadReadyOnlyForm(executionContext);
+                break;
+            case RSMNG.Global.CRM_FORM_TYPE_QUICKCREATE:
+                _self.onLoadCreateForm(executionContext);
+                break;
+        }
+    }
+}
+).call(RSMNG.TAUMEDIKA.SYSTEMUSER);
