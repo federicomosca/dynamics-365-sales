fdac9e562e59a11b99acfae461f2bf0b767e8737
Alessandro Di Balsamo <adibalsamo@alteanet.it>
2024-10-16 16:46:53 +0200

    Aggiornamento funzioni non utilizzate

--- RSMNG.TAUMEDIKA.Plugins/Address/PreCreate.cs
+++ RSMNG.TAUMEDIKA.Plugins/Address/PreCreate.cs
@@ -24,20 +24,6 @@ namespace RSMNG.TAUMEDIKA.Plugins.Address
         }
         public override void ExecutePlugin(CrmServiceProvider crmServiceProvider)
         {
-            void Trace(string key, object value)
-            {
-                //TRACE TOGGLE
-                bool isTraceActive = false;
-                {
-                    if (isTraceActive)
-                    {
-                        key = string.Concat(key.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString())).ToUpper();
-                        value = value.ToString();
-                        crmServiceProvider.TracingService.Trace($"{key}: {value}");
-                    }
-                }
-            }
-
             Entity target = (Entity)crmServiceProvider.PluginContext.InputParameters["Target"];
 
             target.TryGetAttributeValue<EntityReference>(DataModel.res_address.res_customerid, out EntityReference erCustomer);
